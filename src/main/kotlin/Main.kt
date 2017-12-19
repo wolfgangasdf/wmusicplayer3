0eeb2cd5939921653ce504dfd289cc7668bbddb2
wolfgang <wolfgang.loeffler@gmail.com>
2017-12-19 10:02:20 +0100

    cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,6 @@ import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.servlet.ServletHolder
 import org.eclipse.jetty.util.resource.Resource
 
-// devbranch chenge2 change3 change 4
-
-// master change change2
 
 fun main(args: Array<String>) {
 
